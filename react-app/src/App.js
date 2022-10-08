83706ea9aed9a49ac368d800899369a59bd7a6a5
Andy Pham <phamandy00@gmail.com>
2022-10-07 22:13:43 -0400

    Upload initial react files

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import { Button } from "react-bootstrap";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+     <Button>Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
